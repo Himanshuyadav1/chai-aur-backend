411bd2c9358cb5fe38a3cc9de1aa1109ea67489b
Himanshu Yadav <himanshu.tj@gmail.com>
2023-11-22 23:56:36 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -8,18 +8,25 @@ const app = express();
 app.use(cors({ 
     origin: process.env.CORS_ORIGIN,
     credentials: true
- }));
+}));
 
- //for parsing data from FE
- app.use(express.json({ limit: "16kb" }));
+//for parsing data from FE
+app.use(express.json({ limit: "16kb" }));
 
- //for url encoding
- app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+//for url encoding
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
- //for serving files from static folder
- app.use(express.static("public"));
+//for serving files from static folder
+app.use(express.static("public"));
 
- //for cookies
- app.use(cookieParser());
+//for cookies
+app.use(cookieParser());
+
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+app.use("/api/v1/user", userRouter);
 
 export default app;
