3ca36e53da60c64ceb539d461ded0227cad04de6
Himanshu Yadav <himanshu.tj@gmail.com>
2023-11-24 03:54:35 +0530

    Fix all register user and Api Error bugs

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -19,6 +19,10 @@ const uploadOnCloudinary = async (localFilePath) => {
 
         // File has been uploaded successfully
         console.log("File is uploaded on cloudinary ", response.url);
+
+        // remove the locally saved temporary file as the upload operation got successed.
+        fs.unlinkSync(localFilePath);
+
         return response;
 
     } catch (error) {
